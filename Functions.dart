859cb8e5b5c7d1ae99ac501140ecee9f05fc9b50
Najla AlKhaled <najlaalkhaled@Najlas-MacBook-Pro.local>
2022-06-09 20:44:50 +0300

    First Push

---
+++ Functions.dart
@@ -0,0 +1,40 @@
+//void main() {
+//String firstName = "Najla";
+//int age = 28;
+// String lastName = "AlKhaled";
+//  printInfo(first: firstName, last: lastName);
+}
+
+//void printName({required String firstName,String lastName) {
+  //print("Hello my name is $firstName $lastName");
+
+  //void printInfo ({String firstName, int age, String job}) {
+    //print("My name is $firstName and I am $age and I work as a $job");
+  }
+}
+
+//  void sub(int x , int y){
+  // print(y/ x);
+  // }
+
+
+
+
+sumAge(years, age);
+}
+
+
+//Return type functionName(){}
+int sumAge(int years, int age) {
+  return age + years;
+}
+
+
+//What does it return overall?
+int years = 5; 
+int getAge(int age, int years) {
+
+  }
+}
+
+void getAge(int age, int years) {}
